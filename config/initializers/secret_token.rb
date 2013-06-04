685066bd6de53a20c57e279596fe0031f9e2646f
Kensuke Nagae <kyanny@gmail.com>
2013-06-04 21:48:54 +0900

    rails new --skip-active-record

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mongobone::Application.config.secret_token = 'b8bd1d87b7c88cc3f192320ce25eb409a42a53fb1cbdee3e44dc2e0769ab4825d9d11289498530e3329eaa8fdea3fde2983e12e8b6656eebeeed388820240118'
